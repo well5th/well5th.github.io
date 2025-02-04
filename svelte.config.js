2ea2a91109e1feae9bc3352ca517718e9576666b
well5th <kauewksm@gmail.com>
2025-02-04 10:41:34 -0400

    corrigindo 2

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,12 @@
-import adapter from '@sveltejs/adapter-auto';
-
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+import adapter from '@sveltejs/adapter-static';
+export default {
+  kit: {
+      adapter: adapter({
+          pages: 'build',
+          assets: 'build',
+          fallback: undefined,
+          precompress: false,
+          strict: true
+      })
+  }
 };
-
-export default config;
